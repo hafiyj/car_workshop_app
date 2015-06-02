620b34acf0d108bd5c809bfebefa61a1f11eba75
xymerz1 <xymerz1@gmail.com>
2015-06-02 21:44:34 +0800

    reservation models added

---
+++ db/migrate/20150602134051_create_reservations.rb
@@ -0,0 +1,15 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.string :name
+      t.integer :contact_number
+      t.string :car_model
+      t.string :car_reg_number
+      t.string :service_type
+      t.time :time
+      t.date :date
+
+      t.timestamps null: false
+    end
+  end
+end
