f776a058a5e47a06e045fd8c57253f198ba3156e
Mohamad Fadil <xymerz1@gmail.com>
2015-07-12 15:15:32 +0800

    Add,edit successfully reservation test done

--- test/integration/reservation_edit_test.rb
+++ test/integration/reservation_edit_test.rb
@@ -17,4 +17,33 @@ class ReservationEditTest < ActionDispatch::IntegrationTest
                                                         date: " "}
     assert_template 'reservations/edit'
   end
+
+  test "successful edit" do
+    get edit_reservation_path(@reservation)
+    assert_template 'reservations/edit'
+    name           = "Ali"
+    contact_number = "0192777864"
+    car_model      = "Myvi"
+    car_reg_number = "BEB 1"
+    service_type   = "Major"
+    time           = "03:00AM"
+    date           = "2015-09-09"
+    patch reservation_path(@reservation), reservation: { name:  name,
+                                    contact_number: contact_number,
+                                    car_model:      car_model,
+                                    car_reg_number: car_reg_number,
+                                    service_type: service_type,
+                                    time: time,
+                                    date: date}
+    assert_not flash.empty?
+    assert_redirected_to @reservation
+    @reservation.reload
+    assert_equal name,           @reservation.name
+    assert_equal contact_number, @reservation.contact_number
+    assert_equal car_model ,     @reservation.car_model
+    assert_equal car_reg_number, @reservation.car_reg_number
+    assert_equal service_type,   @reservation.service_type
+    # assert_equal time,           @reservation.time
+    # assert_equal date,           @reservation.date
+  end
 end
--- test/integration/reservations_add_test.rb
+++ test/integration/reservations_add_test.rb
@@ -1,17 +1,31 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class ReservationsAddTest < ActionDispatch::IntegrationTest
-#   test "invalid reservation information" do
-#     get reservation_path
-#     assert_no_difference 'Reservation.count' do
-#       post reservations_path, reservation: { name:  "Example",
-#                                contact_number:      "011-1111121",
-#                                car_model:           "Sample Car",
-#                                car_reg_number:      "Wet69",
-#                                service_type:        "Minor",
-#                                time:                "03:00AM" ,
-#                                date:                "12-11-2015"}
-#     end
-#     assert_template 'reservation/new'
-#   end
-# end
+class ReservationsAddTest < ActionDispatch::IntegrationTest
+  test "valid reservation information" do
+    get new_reservation_path
+    assert_difference 'Reservation.count' do
+      post_via_redirect reservations_path, reservation: { name:  "Example",
+                               contact_number:      "0123456789",
+                               car_model:           "Sample Car",
+                               car_reg_number:      "Wet69",
+                               service_type:        "Minor",
+                               time:                "03:00AM" ,
+                               date:                "2015-11-05"}
+    end
+    assert_template 'reservations/show'
+  end
+
+  test "invalid reservation information" do
+    get new_reservation_path
+    assert_no_difference 'Reservation.count' do
+      post reservations_path, reservation: { name:  " ",
+                               contact_number:      " ",
+                               car_model:           " ",
+                               car_reg_number:      " ",
+                               service_type:        " ",
+                               time:                " " ,
+                               date:                " "}
+    end
+    assert_template 'reservations/new'
+  end
+end
