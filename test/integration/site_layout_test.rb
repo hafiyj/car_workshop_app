129ba62473640d6beec8a0db62fa43633c89b99b
Mac Shu <mac_shu11@hotmail.com>
2015-07-27 13:05:02 +0800

    Finished Site Layout Test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+    def setup
+      @workshop = workshops(:workshop1)
+    end
+
     test "layout links" do
         get root_path
         assert_template 'landing_pages/home'
@@ -14,5 +18,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Sign up")
 
     assert_select "a[href=?]", signup_path
+    end
+
+    test "logged in layout links" do
+      get login_path
+      assert_template 'sessions/new'
+
+      assert_select "a[href=?]", login_path
+      log_in_as(@workshop)
+      get root_path
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", login_path, count: 0
+      assert_select "a[href=?]", logout_path
     end
 end
