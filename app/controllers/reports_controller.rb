0aba02a980138b59f998e9a755fa3301e9baefa2
Mac Shu <mac_shu11@hotmail.com>
2015-08-06 22:13:47 +0800

    Completed Google Chart for Reservation Statistics

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,31 @@
 class ReportsController < ApplicationController
 
   def reservation_report
+    # Fetch Recent 12 months Reservation Data
+    @data = Reservation.where(:created_at => 12.months.ago..Time.now).group_by_month
+
+    @reservation_report = Hash.new ()
+    @key = Array.new
+    @value = Array.new
+    @formatted_date = Array.new
+
+    @data.each do |k,v|
+      @key << k.to_datetime
+      @value << v
+    end
+
+    # Format the date of the fetched data
+    @key.each do |r|
+      temp = r.to_datetime
+      temp = temp.strftime("%B, %Y")
+      temp = temp.to_s
+      @formatted_date << temp
+    end
+
+    # Merge the date and value together back to new hash.
+    for i in 0..@data.length-1 do
+      @reservation_report[@formatted_date.at(i)] = @value.at(i)
+    end
 
   end
 end
