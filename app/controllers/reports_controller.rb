0846e839cbf4a37499e76755ef0871ae14481d0e
Mac Shu <mac_shu11@hotmail.com>
2015-08-13 00:05:30 +0800

    Polished, GChart still not working in Production

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,37 +2,18 @@ class ReportsController < ApplicationController
 
   def reservation_report
     # Fetch Recent 12 months Reservation Data
-    #@data = Reservation.where(:created_at => 12.months.ago..Time.now).group_by_month
+    @data = current_workshop.reservations.where(created_at: 12.months.ago..Time.now)
 
-    # @data = Reservation.select("DISTINCT ON (reservation.id) * ")
-    #                    .where(created_at: 12.months.ago..Time.now)
-    #                    .group('date(created_at)').size
-    #@rdata = Reservation.where(created_at: 12.months.ago..Time.now)
-    #@rdata2 = @rdata.group('date(created_at)').size
-    #@data = @rdata.group_by_month
-    @data = current_workshop.reservations.select("DISTINCT ON (reservation.created_at) * ")
-                                         .where(created_at: 12.months.ago..Time.now)
-                                         .group('date(created_at)').size
+    @groupped_data = @data.group('date(created_at)').size
 
-    # @data = case connection.adapter_name
-    #   when 'SQLite'
-    #     Dateslices::Sqlite.time_filter(column, field)
-    #   when 'PostgreSQL', 'PostGIS'
-    #     Dateslices::Postgresql.time_filter(column, field)
-    #   when 'MySQL', 'Mysql2'
-    #     Dateslices::Mysql.time_filter(column, field)
-    #   else
-    #     throw "Unknown database adaptor #{connection.adapter_name}"
-    # end
-    #@data3 = Reservation.all.group_by { |u| u.created_at.month }
-
+    #@groupped_data = @data.group_by_month(:created_at)
 
     @reservation_report = Hash.new ()
     @key = Array.new
     @value = Array.new
     @formatted_date = Array.new
 
-    @data.each do |k,v|
+    @groupped_data.each do |k,v|
       @key << k.to_datetime
       @value << v
     end
@@ -46,7 +27,7 @@ class ReportsController < ApplicationController
     end
 
     # Merge the date and value together back to new hash.
-    for i in 0..@data.length-1 do
+    for i in 0..@groupped_data.length-1 do
       @reservation_report[@formatted_date.at(i)] = @value.at(i)
     end
 
