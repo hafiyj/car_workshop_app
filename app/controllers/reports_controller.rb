02e5b9d73512d15e1cfb17b11d775daf9a2aa930
Mac Shu <mac_shu11@hotmail.com>
2015-08-14 19:48:14 +0800

    Polishing Dashboard and Google Chart

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,20 +3,18 @@ class ReportsController < ApplicationController
   def reservation_report
     # Fetch Recent 12 months Reservation Data
     @data = current_workshop.reservations.unscoped.
-    where(created_at: 12.months.ago..Time.now, workshop_id: current_workshop.id)
-
+            where(created_at: 12.months.ago..Time.now, workshop_id: current_workshop.id)
     @groupped_data = @data.group_by_month
-
+    # Declarations
     @reservation_report = Hash.new ()
     @key = Array.new
     @value = Array.new
     @formatted_date = Array.new
-
+    # Split the data and put into hash
     @groupped_data.each do |k,v|
       @key << k.to_datetime
       @value << v
     end
-
     # Format the date of the fetched data
     @key.each do |r|
       temp = r.to_datetime
@@ -24,11 +22,13 @@ class ReportsController < ApplicationController
       temp = temp.to_s
       @formatted_date << temp
     end
-
     # Merge the date and value together back to new hash.
     for i in 0..@groupped_data.length-1 do
       @reservation_report[@formatted_date.at(i)] = @value.at(i)
     end
 
+    @data = current_workshop.reservations.unscoped.
+                        where(workshop_id: current_workshop.id)
+    @reservation_type = @data.group(:service_type).count
   end
 end
