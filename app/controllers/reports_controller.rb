40840172b38ac5c33603d01aeb87c31cca20c6aa
Mac Shu <mac_shu11@hotmail.com>
2015-08-16 23:14:43 +0800

    Added Appointment Table

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -27,12 +27,42 @@ class ReportsController < ApplicationController
       @reservation_report[@formatted_date.at(i)] = @value.at(i)
     end
 
+    # Fetch the data out from database for the Pie Chart (Service Type)
     @t_data = current_workshop.reservations.unscoped.
                         where(workshop_id: current_workshop.id)
     @reservation_type = @t_data.group(:service_type).count
+
+
+    #Fetch the data out form database for the Area Chart (Service Time)
+    @time_data = current_workshop.reservations.unscoped.
+                        where(workshop_id: current_workshop.id)
+    @time_data = @time_data.group_by_hour(:time)
+
+    @reservation_time = Hash.new ()
+    @t_key = Array.new
+    @t_value = Array.new
+    @f_time = Array.new
+    # Split the data and put into hash
+    @time_data.each do |x,y|
+      @t_key << x.to_time
+      @t_value << y
+    end
+    # Format the date of the fetched data
+    @t_key.each do |r|
+      t = r.utc
+      t = r.strftime('%I:%M %p')
+      t = t.to_s
+      @f_time << t
+    end
+    # Merge the date and value together back to new hash.
+    for i in 0..@time_data.length-1 do
+      @reservation_time[@f_time.at(i)] = @t_value.at(i)
+    end
+
   end
 
   def appointment
+    # Fetch today's appoint from reservation database
     @a_data = current_workshop.reservations.unscoped.
             where(workshop_id: current_workshop.id, date: Date.today)
     @a_groupped_data = @a_data.group_by_hour(:time)
@@ -57,5 +87,10 @@ class ReportsController < ApplicationController
     for i in 0..@a_groupped_data.length-1 do
       @appointment_data[@formatted_time.at(i)] = @a_value.at(i)
     end
+
+    #Fetch the data out from database and use in the Table For
+    @r_data = current_workshop.reservations.unscoped.
+              where(workshop_id: current_workshop.id, date: Date.today)
+    @reserv_data = @r_data.paginate(:per_page => 10, :page => params[:page])
   end
 end
