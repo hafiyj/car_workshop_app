9b9c77eadd188f1841bfa5459bf79fdaecfe8248
Mac Shu <mac_shu11@hotmail.com>
2015-08-18 11:34:56 +0800

    Polished and Tweak the Appointment

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,59 +4,45 @@ class ReportsController < ApplicationController
     # Fetch Recent 12 months Reservation Data
     @r_data = current_workshop.reservations.unscoped.
             where(created_at: 12.months.ago..Time.now, workshop_id: current_workshop.id)
-    @groupped_data = @r_data.group_by_month
+    @r_data = @r_data.group_by_month
     # Declarations
-    @reservation_report = Hash.new ()
+    @reservation_data = Hash.new ()
     @key = Array.new
     @value = Array.new
-    @formatted_date = Array.new
-    # Split the data and put into hash
-    @groupped_data.each do |k,v|
-      @key << k.to_datetime
+    # Split the data and put into hash, and format the date of the fetched data
+    @r_data.each do |k,v|
+      @key << k.to_datetime.strftime("%B, %Y")
       @value << v
     end
-    # Format the date of the fetched data
-    @key.each do |r|
-      temp = r.to_datetime
-      temp = temp.strftime("%B, %Y")
-      temp = temp.to_s
-      @formatted_date << temp
-    end
     # Merge the date and value together back to new hash.
-    for i in 0..@groupped_data.length-1 do
-      @reservation_report[@formatted_date.at(i)] = @value.at(i)
+    for i in 0..@r_data.length-1 do
+      @reservation_data[@key.at(i)] = @value.at(i)
     end
 
+    ############################################################################
     # Fetch the data out from database for the Pie Chart (Service Type)
-    @t_data = current_workshop.reservations.unscoped.
-                        where(workshop_id: current_workshop.id)
-    @reservation_type = @t_data.group(:service_type).count
+    @ty_data = current_workshop.reservations.unscoped.
+              where(workshop_id: current_workshop.id)
+    @type_data = @ty_data.group(:service_type).count
 
-
+    ############################################################################
     #Fetch the data out form database for the Area Chart (Service Time)
-    @time_data = current_workshop.reservations.unscoped.
+    @ti_data = current_workshop.reservations.unscoped.
                         where(workshop_id: current_workshop.id)
-    @time_data = @time_data.group_by_hour(:time)
+    @ti_data = @ti_data.group_by_hour(:time)
 
-    @reservation_time = Hash.new ()
+    @time_data = Hash.new ()
     @t_key = Array.new
     @t_value = Array.new
     @f_time = Array.new
     # Split the data and put into hash
-    @time_data.each do |x,y|
-      @t_key << x.to_time
+    @ti_data.each do |x,y|
+      @t_key << x.to_time.utc.strftime('%I:%M %p').to_s
       @t_value << y
     end
-    # Format the date of the fetched data
-    @t_key.each do |r|
-      t = r.utc
-      t = r.strftime('%I:%M %p')
-      t = t.to_s
-      @f_time << t
-    end
     # Merge the date and value together back to new hash.
-    for i in 0..@time_data.length-1 do
-      @reservation_time[@f_time.at(i)] = @t_value.at(i)
+    for i in 0..@ti_data.length-1 do
+      @time_data[@t_key.at(i)] = @t_value.at(i)
     end
 
   end
